499fb44efefa50655887e732f07da420ac82f528
eurzua <eurzua@umd.edu>
2021-03-26 18:55:14 -0400

    i don't know what I'm doing

--- public/scripts.js
+++ public/scripts.js
@@ -1,40 +1,48 @@
-async function populateRestaurants() {
-  const diningRequest = await fetch('/api/dining');
-  const diningData = await diningRequest.json();
+/*async function getData() {
+  const response = await fetch('/api/dining');
+  const alpha = await response.json();
+  const dining = alpha.data;
+  return dining;
+}
 
-  diningData.data.forEach((restaurant) => {
-    const appendItem = document.createElement('tr');
-    appendItem.classList.add('title', 'has-text-centered', 'is-parent', 'is-3');
-    appendItem.innerHTML = `
-        <article class ="title is-child box has-background-link-dark ">
-        <span class="subtitle has-text-light has-text-weight-bold ">
-        ${restaurant.hall_name} </span>
-        <br />
-        <span class="has-text-light"> ${restaurant.hall_address.split(',')[0]} </span> <br />
-        <span class="has-text-light"> ${restaurant.hall_address.split(',')[1]} </span>
-         </article>`;
-    targetBox.append(appendItem);
+
+function diningHall() {
+  const bravo = document.querySelector('.target')
+  dining.forEach((hall) => {
+    const charlie = document.createElement("tr")
+    charlie.innerHTML = `
+    <td>${element.hall_id}</td>
+    <td>${element.hall_name}</td>
+    <td>${element.hall_address} </td>`;
+    
+    bravo.append(charlie)
   });
+
 }
 
-async function getData() {
-  const diningRequest = await fetch('/api/dining');
-  const diningData = await diningRequest.json();
-  return diningData;
+window.onload = getData();*/
+
+
+
+async function dinner () {
+  const dinnerFetch = await fetch('/api/dining');
+  const dinnerInfo = await dinnerFetch.json();
+  const arrayDinner = dinnerInfo.data; 
+  const information = document.querySelector('.target');
+
+  arrayDinner.forEach((element) => {
+      const makeRows = document.createElement('tr');
+      makeRows.innerHTML = `
+              <td>${element.hall_id}</td>
+              <td>${element.hall_name}</td>
+              <td>${element.hall_address}</td>
+          </tr>      
+      `;
+      information.append(makeRows)
+  });
+  
+  
+
 }
 
-async function setBasicData() {
-  localStorage.setItem('myCat', 'Tom');
-}
-
-async function getBasicData() {
-  const cat = localStorage.getItem('myCat');
-}
-
-async function windowActions() {
-  console.log('loaded window');
-  const data = await getData();
-  const alpha = populateRestaurants();
-}
-
-window.onload = windowActions;
+window.onload = dinner();
